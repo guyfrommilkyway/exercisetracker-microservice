9580f9949e9b5b3bac2b491700acec9cb83e042b
Almer Tampus <almerflorestampus@gmail.com>
2024-02-06 17:51:34 +0800

    Update from String to Date type

--- src/api/v1/models/Exercise.js
+++ src/api/v1/models/Exercise.js
@@ -4,7 +4,7 @@ const ExerciseSchema = new Schema({
 	username: { type: Schema.Types.ObjectId, ref: 'User' },
 	description: { type: String, required: true },
 	duration: { type: Number, required: true },
-	date: { type: String, required: true },
+	date: { type: Date, required: true },
 });
 
 const Exercise = model('Exercise', ExerciseSchema);
