41323fa10819912306de5a520500d889d3f7b633
Almer Tampus <almerflorestampus@gmail.com>
2024-02-06 17:52:36 +0800

    Add logic for query parameters

--- src/api/v1/services/Log.js
+++ src/api/v1/services/Log.js
@@ -1,6 +1,6 @@
 const Log = require('../models/Log');
 
-const getLogs = async (userId) => {
+const getLogs = async (userId, from, to, limit) => {
 	try {
 		const logs = await Log.findOne({ username: userId })
 			.populate({ path: 'username', transform: (user) => user.username })
@@ -14,6 +14,32 @@ const getLogs = async (userId) => {
 				},
 			})
 			.lean();
+
+		const isValidFrom = (date) => date >= new Date(from);
+		const isValidTo = (date) => date <= new Date(to);
+
+		if (from && to) {
+			logs.log = logs.log.filter(
+				(log) =>
+					isValidFrom(log.date) &&
+					isValidTo(log.date) &&
+					log.date.toDateString()
+			);
+		}
+
+		if (from && !to) {
+			logs.log = logs.log.filter(
+				(log) => isValidFrom(log.date) && log.date.toDateString()
+			);
+		}
+
+		if (!from && to) {
+			logs.log = logs.log.filter(
+				(log) => isValidTo(log.date) && log.date.toDateString()
+			);
+		}
+
+		logs.log = limit ? logs.log.slice(0, limit) : logs.log;
 
 		return logs;
 	} catch (e) {
